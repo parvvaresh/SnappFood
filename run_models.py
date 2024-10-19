8b68ce7346d657a3c74d0d022217c49873dcc649
reza <parvvaresh@gmail.com>
2024-10-19 15:12:14 +0330

    fix bufs : fix name moudles based on Ml alg

--- run_models.py
+++ run_models.py
@@ -1,12 +1,14 @@
-from models.train_models import train_MLmodels
+from models.train_MLmodels import train_MLmodels
 import pandas as pd
 from PreProcess import PreProcess
 
 def main():
     df = pd.read_csv("./data/Snappfood.csv")
-    result = train_models(df, PreProcess)
+    result = train_MLmodels(df, PreProcess)
     result.to_csv("./assets/result_ml.csv")
     print(f"result saved in <<./assets/result_ml.csv>>")
+    
+    
     
 
 main()
