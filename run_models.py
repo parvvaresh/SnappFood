f4de2dc04dd83b4dc132124ffcd92a2c0fb6cdda
parvvaresh <parvvaresh@gmail.com>
2025-01-02 13:59:01 +0330

    fix bugs :: fix import package

--- run_models.py
+++ run_models.py
@@ -1,10 +1,10 @@
-from models.train_MLmodels import train_MLmodels
+from models.train_MLmodels import train_models
 import pandas as pd
 from PreProcess import PreProcess
 
 def main():
     df = pd.read_csv("./data/Snappfood.csv")
-    result = train_MLmodels(df, PreProcess)
+    result = train_models(df, PreProcess)
     result.to_csv("./assets/result_ml.csv")
     print(f"result saved in <<./assets/result_ml.csv>>")
     
